73fd268391bf61ef920040a8d6fbae27015e43b5
bpafoshizle <bpafoshizle@gmail.com>
2023-06-17 13:21:38 -0500

    fixing some bugs to deploy readarr. updated ingress

--- kube/media/05-install-readarr.sh
+++ kube/media/05-install-readarr.sh
@@ -1,7 +1,7 @@
 helm repo add home-kube-media https://bpafoshizle.github.io/home-kube-media
 helm repo update
 
-helm install radarr home-kube-media/readarr \
+helm install readarr home-kube-media/readarr \
     --values ../kube/media/media.readarr.values.yml \
     --namespace media
 
